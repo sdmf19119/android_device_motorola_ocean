271a276bf645449312f5f5a6fda7e3e5433ae94c
Geofferey Eakins <geofferey.eakins@gmail.com>
2019-10-17 20:17:49 -0700

    ocean: include dummy init.mmi.hab.sh

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,3 +11,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.mmi.hab.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.mmi.hab.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
